a54cfed32aa73c07846f8e83996c9f2e7ba6f0bd
doankhiem <khiembi161219@gmail.com>
2024-12-11 21:41:47 +0700

    edit

---
+++ components/Portfolio.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class=" max-w-6xl mx-auto py-5 px-4 md:px-0 gap-10">
+        <div class="grid md:grid-cols-2 grid-cols-1 relative border-2 rounded-3xl mt-5 border-[#d7a947] p-5 md:col-span-3">
+            <div class="">
+                <img src="~/assets/images/three-dot.png" alt="Image Avatar" class="w-32 absolute -top-4 right-20">
+                <h2
+                    class="absolute -top-3 right-1/2 px-10 pacifico-regular-font text-3xl border-2 rounded-full border-primary bg-white">
+                    Boi Boi Truong</h2>
+                <h1 class="text-primary abril-fatface-regular-font text-9xl">Port</h1>
+                <h1 class="abril-fatface-regular-font text-9xl">Folio</h1>
+                <div>
+                    <span class="crimson-pro-font border-2 rounded-full px-4 border-primary text-4xl">Communication
+                        Portfolio</span>
+                </div>
+                <div class="mt-5">
+                    <p>If opportunity doesn't knock, build a portfolio When it does, knock me at:</p>
+                </div>
+                <div class="mt-5">
+                    <div class="bg-primary inline-flex rounded-full px-5 py-1">
+                        <img class="w-[26px] mr-2" src="~/assets/images/mail.png" alt="Icon Email">
+                        <p class="text-white ">truongboiboipy123@gmail.com</p>
+                    </div>
+                </div>
+                <div class="mt-2">
+                    <div class="bg-primary inline-flex rounded-full px-5 py-1">
+                        <img class="w-[26px] mr-2" src="~/assets/images/phone.png" alt="Icon Email">
+
+                        <p class="text-white">(+84) 905146898</p>
+                    </div>
+                </div>
+                <div class="mt-2">
+                    <div class="bg-primary inline-flex rounded-full px-5 py-1">
+                        <img class="w-[26px] mr-2" src="~/assets/images/address.png" alt="Icon Email">
+                        <p class="text-white">Phu Yen, Vietnam</p>
+                    </div>
+                </div>
+                <div class="mt-2">
+                    <div class="bg-primary inline-flex rounded-full px-5 py-1">
+                        <img class="w-[26px] mr-2" src="~/assets/images/birthday.png" alt="Icon Email">
+                        <p class="text-white">22th, June, 2007</p>
+                    </div>
+                </div>
+            </div>
+            <!-- <div class=" bg-primary"> -->
+                <img src="~/assets/images/avt-1.png" alt="Image Avatar"
+                    class="w-full object-cover rounded-lg transition-transform duration-300 hover:scale-105">
+            <!-- </div> -->
+        </div>
+        <!-- <div class="md:col-span-2 relative bg-primary">
+            <img src="~/assets/images/avt-1.png" alt="Image Avatar"
+                    class="w-full  object-cover rounded-lg transition-transform duration-300 hover:scale-105">
+        </div> -->
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
