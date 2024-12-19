0d9f9b2a9c2f337dcf6564daa19e05720fddb144
doankhiem <khiembi161219@gmail.com>
2024-12-19 21:51:04 +0700

    WORK EXPERIENCE

--- components/WorkExperience.vue
+++ components/WorkExperience.vue
@@ -1,56 +1,19 @@
 <template>
     <div class="bg-[#fff5f0cf] p-5 rounded-xl mt-5">
         <h1 class="text-primary abril-fatface-regular-font text-5xl md:text-6xl uppercase mb-5">WORK EXPERIENCE</h1>
-        <!-- <div class="mx-auto px-5">
-            <ul class="list-disc text-xl leading-10 italic font-medium">
-                <li>MC for the event: Mid-Autumn Festival for children</li>
-                <li>Forum host: Building beautiful friendships, saying no to school violence</li>
-                <li>MC of the historical and cultural event at school</li>
-                <li>MC of the 2024 school year closing ceremony </li>
-                <li>Character experience of a PTP news program</li>
-            </ul>
-        </div> -->
 
-        <div
+        <!-- <div
             class="space-y-8 relative before:absolute before:inset-0 before:ml-[0.6rem] before:-translate-x-px md:before:ml-[19rem] md:before:translate-x-0 before:top-4 before:h-[100% - 1rem] before:w-1 before:bg-gradient-to-b before:from-transparent before:bg-[#d7a947] before:to-transparent">
-
-            <!-- Item #1 -->
             <div class="relative">
                 <div class="md:flex items-center md:space-x-4 mb-3">
                     <div class="flex items-center space-x-4 md:space-x-2 md:space-x-reverse">
-                        <!-- Icon -->
                         <div
                             class="flex items-center justify-center w-5 h-5 rounded-full bg-[#d7a947] shadow md:order-1">
-                            <!-- <svg class="fill-[#d7a947]" xmlns="http://www.w3.org/2000/svg" width="16" height="16">
-                            <path d="M8 0a8 8 0 1 0 8 8 8.009 8.009 0 0 0-8-8Zm0 12a4 4 0 1 1 0-8 4 4 0 0 1 0 8Z" />
-                        </svg> -->
                         </div>
-                        <!-- Date -->
                         <time class="font-caveat font-medium text-xl text-white text-end md:w-72">
-                            <!-- <h1 class="bg-primary rounded-full px-5 py-1 inline">2022 - Present</h1> -->
                         </time>
                     </div>
-                    <!-- Title -->
-                    <!-- <div class="text-slate-500 ml-14">
-                        <span class="text-slate-900 font-bold">Ngo Gia Tu High School</span>
-                    </div> -->
                 </div>
-                <!-- Card -->
-                <!-- <div class="bg-white p-4 rounded border border-slate-200 text-slate-500 shadow ml-14 md:ml-[350px] mb-2">
-                Social Specialization
-            </div>
-            <div class="bg-white p-4 rounded border border-slate-200 text-slate-500 shadow ml-14 md:ml-[350px] mb-2">
-                GPA: 8.9/10 (top 3 in class)
-            </div>
-            <div class="bg-white p-4 rounded border border-slate-200 text-slate-500 shadow ml-14 md:ml-[350px] mb-2">
-                3rd place in English Excellent Students Contest 2023
-            </div>
-            <div class="bg-white p-4 rounded border border-slate-200 text-slate-500 shadow ml-14 md:ml-[350px] mb-2">
-                "Hoc sinh 3 Tot" Award (Excellent Student Award) 2023
-            </div>
-            <div class="bg-white p-4 rounded border border-slate-200 text-slate-500 shadow ml-14 md:ml-[350px] mb-2">
-                Member of the Standing Committee of the School Youth Union in 2023 and 2024
-            </div> -->
 
                 <div class="px-5 ml-14 md:ml-[350px]">
                     <ul class="list-disc text-xl leading-10 italic">
@@ -65,6 +28,104 @@
 
 
 
+        </div> -->
+
+        <div class="grid md:grid-cols-3 p-4 bg-primary">
+            <div class="md:col-span-1">
+                <div class="inline-flex">
+                    <img src="~/assets/images/star-6.png" alt="Image Avatar" class="w-5 h-5">
+                    <p class="text-white yeseva-one-font font-bold ml-2">MC for Mid-Autumn Festival for children</p>
+                </div>
+                <div class="px-5">
+                    <ul class="list-disc text-xl text-white">
+                        <li>Hosted and managed the event program, ensuring a lively and engaging atmosphere.</li>
+                        <li>Hosted and managed the event program, ensuring a lively and engaging atmosphere.</li>
+                    </ul>
+                </div>
+                <img src="~/assets/images/we-1.png" alt="Image Avatar" class="w-full object-cover ">
+            </div>
+            <div class="md:col-span-1">
+                <div class="inline-flex">
+                    <img src="~/assets/images/star-6.png" alt="Image Avatar" class="w-5 h-5">
+                    <p class="text-white yeseva-one-font font-bold ml-2">Forum host: Building beautiful friendships, saying
+                        no to school violence</p>
+                </div>
+                <div class="px-5">
+                    <ul class="list-disc text-xl text-white">
+                        <li>Facilitated discussions on fostering positive relationships and raising awareness about
+                            school violence prevention.</li>
+                        <li>Collaborated with speakers and organizers to ensure a smooth and impactful event.</li>
+                    </ul>
+                </div>
+                <img src="~/assets/images/we-2.png" alt="Image Avatar" class="w-full object-cover ">
+            </div>
+            <div class="md:col-span-1">
+                <div class="inline-flex">
+                    <img src="~/assets/images/star-6.png" alt="Image Avatar" class="w-5 h-5">
+                    <p class="text-white yeseva-one-font font-bold ml-2">Character experience of a PTP news program</p>
+                </div>
+                <div class="px-5">
+                    <ul class="list-disc text-xl text-white">
+                        <li>Participated as a featured character, sharing personal stories and experiences that aligned
+                            with the program's theme.</li>
+                        <li>Developed confidence in media presence and storytelling, enhancing communication and public
+                            speaking skills.</li>
+                    </ul>
+                </div>
+                <img src="~/assets/images/we-3.png" alt="Image Avatar" class="w-full object-cover ">
+            </div>
+        </div>
+
+        <div class="grid md:grid-cols-5 p-4">
+            <div class="md:col-span-3">
+                <div class="bg-primary inline-flex rounded-full px-5 py-1">
+                    <p class="text-white nunito-font font-bold">MC for closing ceremony of my school<br>Period: May 2024
+                    </p>
+                </div>
+                <div class="px-5">
+                    <ul class="list-disc text-xl leading-10 text-[#dbb055]">
+                        <li>Hosted the event, creating an engaging and memorable experience for students and faculty.
+                        </li>
+                        <li>Coordinated seamlessly with a well-known KOL, model, and also a my shool’s alumni, ensuring
+                            their participation added value to the ceremony.</li>
+                        <li>Developed strong communication skills by interacting with the celebrity guest and managing
+                            the event’s flow.</li>
+                        <li>Contributed to the overall success of the ceremony, making it a meaningful celebration of
+                            the school year.</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="md:col-span-2">
+                <img src="~/assets/images/we-4.png" alt="Image Avatar"
+                    class="w-full object-cover mb-4 transition-transform duration-300 hover:scale-105">
+                <img src="~/assets/images/we-5.png" alt="Image Avatar"
+                    class="w-full object-cover mb-4 transition-transform duration-300 hover:scale-105">
+            </div>
+        </div>
+
+        <div class="grid md:grid-cols-5 p-4">
+            <div class="md:col-span-2">
+                <img src="~/assets/images/we-6.png" alt="Image Avatar"
+                    class="w-full object-cover mb-4 transition-transform duration-300 hover:scale-105">
+
+            </div>
+            <div class="md:col-span-3">
+                <div class="bg-primary inline-flex rounded-full px-5 py-1">
+                    <p class="text-white nunito-font font-bold">MC of the historical and cultural event at school</p>
+                </div>
+                <div class="px-5">
+                    <p class="text-xl text-[#dbb055]">
+                        As the MC for the Historical and Cultural event at school, I had the opportunity to lead a
+                        meaningful program, connecting activities such as minigames, traditional costume presentations,
+                        and the award ceremony for the winning class. I not only took on the role of the host but also
+                        created an engaging space that sparked interest and enthusiasm among the students. Introducing
+                        and explaining the significance of the traditional costumes helped everyone appreciate and
+                        understand our national culture more deeply. The event was not only an opportunity to showcase
+                        my creativity as an MC but also allowed me to enhance my communication skills and ability to
+                        work under pressure.
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
