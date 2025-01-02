8494b7bf1d0e1488b11d82d0f136fced84555450
khiemhome <khiembi161219@gmail.com>
2025-01-02 13:55:08 +0700

    detail project (YEMUN, LittleReportersClub)

---
+++ pages/project-detail/LittleReportersClub.vue
@@ -0,0 +1,99 @@
+<template>
+    <div class="mx-auto py-5 px-4">
+        <div class="bg-[#fff5f0cf] p-5 rounded-xl mt-5 text-xl">
+            <p class="text-center">
+                We’ve all heard someone hum this saying: “Every person’s life is a jigsaw puzzle, and every piece has a
+                reason to exist."
+                Indeed, you and Little Reporter have found each other, and from this moment on, you will be pieces of
+                each other's puzzle. A “key” piece holds countless surprises yet to be revealed, waiting for you to
+                unlock the door and embark on a meaningful journey together.
+                Welcome to the Little Reporter family!
+            </p>
+            <div class="grid grid-cols-2 md:grid-cols-3 gap-1 md:gap-5 mt-10">
+                <img class="col-span-1 order-2 md:order-1"
+                    src="~/assets/images/projects/LittleReportersClub/LienHoanCLB/1.jpg" alt="">
+                <p class="col-span-2 md:col-span-1 order-1 md:order-2">
+                    The Phu Yen Province Club - Team - Group Festival was a meaningful day, bringing together over 100
+                    students to participate in bonding activities. The morning featured dynamic games such as group flag
+                    decoration, ball passing, intellectual challenges, and "Ring the Golden Bell." These warm-up
+                    activities created a cheerful atmosphere, bringing participants closer together and transforming
+                    strangers into friends.
+                </p>
+                <img class="col-span-1 order-3 md:order-3"
+                    src="~/assets/images/projects/LittleReportersClub/LienHoanCLB/2.jpg" alt="">
+            </div>
+
+            <p class="mt-10 text-center">
+                On October 25, 2024, the Little Reporter Club was honored to attend the Volunteer Collaboration
+                Conference of Phu Yen Radio and Television Station.
+                <br>
+                <br>
+                The club had the opportunity to meet the station's collaborators, listen to their insights,
+                directions, and the hopes and aspirations of the leadership. Additionally, we were moved by a very
+                touching song that inspired everyone in the audience with a spirit of "love for the country -
+                compassion for others," leaving us with a deep sense of longing encapsulated in the three words
+                "gratitude for people".
+                <br>
+                <br>
+                Furthermore, we were extremely happy and proud to be one of the seven organizations recognized for
+                outstanding contributions in collaboration during the 2023-2024 year. We hope that in the upcoming
+                2024-2025 period, the Little Reporter Club will continue to grow strong and be a team known for its
+                high-quality publications!
+            </p>
+            <div class="grid md:grid-cols-2 gap-1 md:gap-5 mt-5">
+                <img class="md:col-span-1 " src="~/assets/images/projects/LittleReportersClub/HoiNghiCTV/1.jpeg" alt="">
+                <img class="md:col-span-1 " src="~/assets/images/projects/LittleReportersClub/HoiNghiCTV/2.jpeg" alt="">
+            </div>
+
+
+            <p class="mt-10 text-center">
+                On February 7th, the Little Reporter Club organized a charity event to distribute Tet gifts, including
+                packs of candies and essential goods, to those in need in Tuy Hoa City. The funds for this event came
+                from the sale of lucky money envelopes and bracelets that we had organized earlier. Each gift given was
+                wrapped with a bit of our love, and at the same time, it served as a seed—small, yet we believe it will
+                bring happiness to the elderly and the disadvantaged during this Lunar New Year.
+                <br>
+                <br>
+                Sometimes, doing good is not as difficult as you think. Just a small word of encouragement can bring joy
+                and warmth to others!
+                <br>
+                Now, let’s take a moment to look back at the memorable moments we've shared!
+            </p>
+            <div class="grid  md:grid-cols-3 grid-cols-2 gap-1 md:gap-5 mt-5">
+                <img class="col-span-1 " src="~/assets/images/projects/LittleReportersClub/TuThien/1.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/LittleReportersClub/TuThien/2.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/LittleReportersClub/TuThien/3.jpg" alt="">
+                <img class="col-span-1" src="~/assets/images/projects/LittleReportersClub/TuThien/4.jpg" alt="">
+                <img class="col-span-1" src="~/assets/images/projects/LittleReportersClub/TuThien/5.jpg" alt="">
+                <img class="col-span-1" src="~/assets/images/projects/LittleReportersClub/TuThien/6.jpg" alt="">
+                <img class="col-span-1" src="~/assets/images/projects/LittleReportersClub/TuThien/7.jpg" alt="">
+                <img class="col-span-1" src="~/assets/images/projects/LittleReportersClub/TuThien/8.jpg" alt="">
+                <img class="col-span-1" src="~/assets/images/projects/LittleReportersClub/TuThien/9.jpg" alt="">
+            </div>
+
+            <div class="grid grid-cols-2 md:grid-cols-3 gap-1 md:gap-5 mt-10">
+                <img class="col-span-1 order-2 md:order-1" src="~/assets/images/projects/LittleReportersClub/2.jpg"
+                    alt="">
+                <p class="col-span-2 md:col-span-1 order-1 md:order-2">
+                    On the evening of July 6th, the Little Reporters Club was present at April 1 Square to "engage in
+                    hands-on practice" and gather news and stories from the 27th Pink Lotus Festival in 2024—one of the
+                    major events of the province and the region. During last night's rehearsal, the Little Reporters
+                    collaborated to produce engaging articles, capture stunning photos, and create an intriguing
+                    reportage.
+                    <br>
+                    The Little Reporters Club is honored to be guided by the Youth Activity Center and to independently
+                    gather news during this event.
+                </p>
+                <img class="col-span-1 order-3 md:order-3" src="~/assets/images/projects/LittleReportersClub/3.jpg"
+                    alt="">
+            </div>
+            
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
---
+++ pages/project-detail/YEMUN.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="mx-auto py-5 px-4">
+        <div class="bg-[#fff5f0cf] p-5 rounded-xl mt-5 text-xl">
+            <div class="grid md:grid-cols-3 gap-1 md:gap-5">
+                <p class="md:col-span-1">
+                    <b>Member of Marketing:</b>
+                    The moment has finally arrived. The new generation of YE-MUN members gathered in one place, meeting
+                    each other in person for the first time. Everyone was happy and eagerly anticipating. The sound of
+                    laughter and chatter filled the Sunday morning atmosphere.
+                    <br>
+                    <br>
+                    We sincerely thank you for your enthusiasm. Through this meeting, members will have the opportunity
+                    to talk, understand each other better, become more connected, and together create a new, passionate
+                    organizing team that will help YE-MUN soar high and far once again.
+                </p>
+                <img class="md:col-span-2" src="~/assets/images/projects/YEMUN/MARKETING/2.jpg" alt="">
+            </div>
+
+
+            <p class="mt-10 text-center">
+                So why wait? Let's look back at those sweet moments together!
+            </p>
+            <div class="grid md:grid-cols-3 grid-cols-2 gap-1 md:gap-5 mt-5">
+                <img class="col-span-1 " src="~/assets/images/projects/YEMUN/MARKETING/1.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/YEMUN/MARKETING/5.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/YEMUN/MARKETING/3.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/YEMUN/MARKETING/4.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/YEMUN/MARKETING/6.png" alt="">
+                <img class="col-span-1" src="~/assets/images/projects/YEMUN/MARKETING/7.jpg" alt="">
+            </div>
+
+            <p class="mt-10 text-center">
+                <b>Delegate of UNICEF:</b>
+                Returning once again this year, the YE-MUN 2024 Conference is adorned with a new theme: POSTERITY. Here,
+                young people, with the heart and spirit of the Vietnamese, will debate global issues and bring the
+                national voice to the international stage. Focusing on four main councils: the United Nations General
+                Assembly (UNGA), the United Nations International Children’s Emergency Fund (UNICEF), the United Nations
+                Security Council (UNSC), and the United Nations High Commissioner for Refugees (UNHCR), participants
+                will have the opportunity to demonstrate their mettle, express their individuality, and break free from
+                the constraints that have long held them back.
+            </p>
+
+            <div class="grid md:grid-cols-2 gap-1 md:gap-5 mt-5">
+                <p class="col-span-1">
+                    The first day of departure of the ship named YE-MUN 2024 officially left the station successfully
+                    with emotions full of joy and excitement. "Crews" from all over the S-shaped land have gathered
+                    together, ready for the journey to conquer the vast ocean of knowledge and discover the mysteries of
+                    the night sky.
+                </p>
+                <img class="col-span-1" src="~/assets/images/projects/YEMUN/8.jpg" alt="">
+            </div>
+
+            <p class="mt-10 text-center">
+                Delegates, esteemed chairs, observers, and the organizing team at YE-MUN have always been like this
+                - dedicated and passionately contributing their youthful spirit. And now, those beautiful moments
+                will be captured and preserved forever.
+            </p>
+
+            <div class="grid  md:grid-cols-3 grid-cols-2 gap-1 md:gap-5 mt-5">
+                <img class="col-span-1 " src="~/assets/images/projects/YEMUN/1.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/YEMUN/4.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/YEMUN/13.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/YEMUN/14.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/YEMUN/2.jpg" alt="">
+                <img class="col-span-1" src="~/assets/images/projects/YEMUN/3.jpg" alt="">
+            </div>
+
+            <div class="grid grid-cols-2 md:grid-cols-3 gap-1 md:gap-5 mt-10">
+                <img class="col-span-1 order-2 md:order-1" src="~/assets/images/projects/YEMUN/9.jpg"
+                    alt="">
+                <p class="col-span-2 md:col-span-1 order-1 md:order-2">
+                    On the evening of August 11th, everyone dressed their best, smiled their brightest, and walked into
+                    the prom room with the pride of victory. Applause echoed throughout the room, as everyone engaged in
+                    conversations, introductions, award presentations, and deep emotions, mingled with a hint of radiant
+                    wistfulness. At that moment, everything was incredibly beautiful. It’s a memory that will likely
+                    never fade.
+                </p>
+                <img class="col-span-1 order-3 md:order-3" src="~/assets/images/projects/YEMUN/6.jpg"
+                    alt="">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
