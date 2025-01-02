5039c7adaf9ffd17e0d1bb860ae81e333b635fd6
khiemhome <khiembi161219@gmail.com>
2025-01-02 10:46:04 +0700

    detail project (CAM, DTEC)

---
+++ pages/project-detail/CAM.vue
@@ -0,0 +1,116 @@
+<template>
+    <div class="mx-auto py-5 px-4">
+        <div class="bg-[#fff5f0cf] p-5 rounded-xl mt-5 text-xl">
+            <div class="grid md:grid-cols-3">
+                <p class="col-span-1">
+                    <b>Dong Khong Lanh</b> <br>
+                    On 8th and 9th December, “Dong Khong Lanh”- a fundraising event that has been
+                    cherished for a long time
+                    with the desire to spread “winter warmth” to everyone and together create truly worthwhile moments
+                    was
+                    held, where teenagers can buy environmentally friendly handmade products and enjoy an acoustic music
+                    night. Through the event, we wanted to convey a message about spreading love and warmth to people
+                    around
+                    us more; love has the unimaginable power, it saves humans from a savage life.
+                </p>
+                <video controls class="md:pl-5 col-span-2 mt-2 md:mt-0" autoplay>
+                    <source src="~/assets/images/projects/CAM/SuKienDongKhongLanh/vid1.mp4" type="video/mp4" />
+                </video>
+            </div>
+            <p class="mt-10 text-center">
+                As a leader of the project, I was so relieved that the event went according to plan. We were able to
+                provide everyone with the satisfaction that they had come to expect from the event this year. Here are
+                some moments from the evening.
+            </p>
+            <div class="grid md:grid-cols-3 grid-cols-2 gap-1 md:gap-5 mt-5">
+                <img class="col-span-1 " src="~/assets/images/projects/CAM/SuKienDongKhongLanh/1.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/CAM/SuKienDongKhongLanh/2.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/CAM/SuKienDongKhongLanh/3.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/CAM/SuKienDongKhongLanh/4.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/CAM/SuKienDongKhongLanh/6.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/CAM/SuKienDongKhongLanh/14.jpg" alt="">
+            </div>
+            <p class="mt-10 text-center">
+                The event ended with a number of 80 orders and 7 million VND sponsorships, surpassing our target
+            </p>
+            <div class="grid  md:grid-cols-3 grid-cols-2 gap-1 md:gap-5 mt-5">
+                <img class="col-span-1 " src="~/assets/images/projects/CAM/SuKienDongKhongLanh/7.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/CAM/SuKienDongKhongLanh/12.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/CAM/SuKienDongKhongLanh/15.jpg" alt="">
+                <img class="col-span-1 md:hidden" src="~/assets/images/projects/CAM/SuKienDongKhongLanh/13.jpg" alt="">
+            </div>
+
+
+            <div class="grid md:grid-cols-3 gap-5 mt-10">
+                <p class="md:col-span-2">
+                    <b>Trung Thu Cho Em</b><br>
+                    With the desire to create a warm and joyful Mid-Autumn Festival for the children, warmth that comes
+                    not
+                    only from the lovely lanterns and packets of cakes and candies but also from the heartfelt effort to
+                    create a playground filled with laughter, helping them cherish the most wonderful memories of the
+                    festival night. With these aspirations, CAM collaborated with Xanh Camping to organize an event
+                    where
+                    the children could play freely and enjoy an unforgettable Mid-Autumn night.
+                    <img class="mt-2" src="~/assets/images/projects/CAM/TrungThuChoEm/9.jpg" alt="">
+                </p>
+                <img class="md:col-span-1 " src="~/assets/images/projects/CAM/TrungThuChoEm/8.jpg" alt="">
+            </div>
+
+            <p class="mt-10 text-center">
+                With a total of more than 30 members and 20 volunteers, we were able to get the event well-prepared.
+            </p>
+            <div class="grid  md:grid-cols-3 grid-cols-2 gap-1 md:gap-5 mt-5">
+                <img class="col-span-1 " src="~/assets/images/projects/CAM/TrungThuChoEm/1.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/CAM/TrungThuChoEm/5.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/CAM/TrungThuChoEm/7.jpg" alt="">
+                <img class="col-span-1 md:hidden" src="~/assets/images/projects/CAM/TrungThuChoEm/10.jpg" alt="">
+            </div>
+
+
+            <div class="grid md:grid-cols-3 gap-5 mt-10">
+                <p class="col-span-1 ">We were so fortunate because we got support from local sponsors and partners, who
+                    always tried their
+                    best to make 'Trung Thu Cho Em' a successful event.
+                    <br>
+                    Additionally, thanks to all the volunteers who dynamically took part in the activities. We were glad
+                    that we got positive feedback from local authority, and all the children there
+                </p>
+                <img class="col-span-1 " src="~/assets/images/projects/CAM/TrungThuChoEm/3.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/CAM/TrungThuChoEm/6.jpg" alt="">
+            </div>
+
+            <div class="grid grid-cols-2 md:grid-cols-3 gap-1 md:gap-5 mt-10">
+                <img class="col-span-1 order-2 md:order-1" src="~/assets/images/projects/CAM/XuanTinhNguyen/6.jpg"
+                    alt="">
+                <p class="col-span-2 md:col-span-1 order-1 md:order-2">
+                    <b>Xuan Thien Nguyen</b> <br>
+                    During the time we participated in previous events, we had the opportunity to learn about these
+                    extraordinary individuals. They are the ones who, despite challenges, quietly and selflessly
+                    dedicate themselves to society. This has made us truly appreciate and feel grateful to them,
+                    inspiring us to embrace this project with the hope of sharing a bit of our compassion and bringing
+                    them a warm and joyful Lunar New Year.
+                </p>
+                <img class="col-span-1 order-3 md:order-3" src="~/assets/images/projects/CAM/XuanTinhNguyen/4.jpg"
+                    alt="">
+            </div>
+
+            <p class="mt-10 text-center">
+                Through this event, we not only gave but also received something truly special in return. It was the
+                joyful smiles, the tearful eyes filled with surprise, and the heartfelt words of gratitude, blessings,
+                and warm, happy stories shared with us.
+            </p>
+            <div class="grid md:grid-cols-3 grid-cols-2 gap-1 md:gap-5 mt-5">
+                <img class="col-span-1 " src="~/assets/images/projects/CAM/XuanTinhNguyen/1.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/CAM/XuanTinhNguyen/2.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/CAM/XuanTinhNguyen/5.jpg" alt="">
+                <img class="col-span-1 md:hidden" src="~/assets/images/projects/CAM/XuanTinhNguyen/3.jpg" alt="">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
---
+++ pages/project-detail/DTEC.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="mx-auto py-5 px-4">
+        <div class="bg-[#fff5f0cf] p-5 rounded-xl mt-5 text-xl">
+            <p class="text-center">
+                On the evening of April 14, 2024, the Begin Again concert had gone through incredibly vibrant and
+                wonderful moments. It also marked a significant milestone for the success of the DTEC club after months
+                of dedication and effort. At the Diên Hồng cultural center, all members had the opportunity to showcase
+                their talents and give their all with passion on this stage, and that fiery passion continued to shine
+                brightly.
+            </p>
+            <div class="grid md:grid-cols-3 grid-cols-2 gap-1 md:gap-5 mt-5">
+                <video controls class="col-span-1 mt-2 md:mt-0">
+                    <source src="~/assets/images/projects/DTEC/vid1.mp4" type="video/mp4" />
+                </video>
+                <video controls class="col-span-1 mt-2 md:mt-0">
+                    <source src="~/assets/images/projects/DTEC/vid2.mp4" type="video/mp4" />
+                </video>
+                <img class="col-span-1 " src="~/assets/images/projects/DTEC/1.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/DTEC/3.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/DTEC/4.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/DTEC/5.jpg" alt="">
+            </div>
+            <p class="mt-10 text-center">
+                Especially, as the stage lights shone, the stands were filled with guests who arrived early and eagerly
+                awaited the event to begin. Throughout the concert, the crisp applause, the hearty laughter, the echoing
+                cheers, the tears, the bursting emotions, all of these things created an explosion for the night. And
+                then, as the stage lights gradually dimmed and the curtains began to close, it was also the end of
+                DTEC's journey with "6 months, 1 love" for Begin Again.
+            </p>
+            <div class="grid  md:grid-cols-3 grid-cols-2 gap-1 md:gap-5 mt-5">
+                <img class="col-span-1 " src="~/assets/images/projects/DTEC/6.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/DTEC/7.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/DTEC/12.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/DTEC/9.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/DTEC/13.jpg" alt="">
+                <img class="col-span-1" src="~/assets/images/projects/DTEC/11.jpg" alt="">
+            </div>
+
+            <p class="mt-10 text-center">
+                Let's look back at the countless emotional moments of the Begin Again concert together!
+            </p>
+            <div class="grid  md:grid-cols-3 grid-cols-2 gap-1 md:gap-5 mt-5">
+                <img class="col-span-1 " src="~/assets/images/projects/DTEC/8.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/DTEC/10.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/DTEC/14.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/DTEC/15.jpg" alt="">
+                <img class="col-span-1 " src="~/assets/images/projects/DTEC/16.jpg" alt="">
+                <img class="col-span-1" src="~/assets/images/projects/DTEC/17.jpg" alt="">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
